299f5a73059dc686a0d03a928b74cdc07f6a8255
Ivan De Alba <ivan_dealba@outlook.com>
2019-06-26 20:48:55 -0500

    Created a variable to store alphabet letter as its respective ASCII code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,4 +3,11 @@ $(document).ready(function(){
     $(document).on('keypress', function(event){
         $('#userGuesses').append(` ${event.key},`);
     })
+
+    // ASCII code for alphabet (a-z = 65-90)
+    var correctNumber = Math.floor(Math.random() * 25) + 65;
+
+    for (var i = 0; i < 9; i++){
+
+    }
 })
