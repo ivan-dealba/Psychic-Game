b24fddf859c216bd849f614a6415b34c4710867a
Ivan De Alba <ivan_dealba@outlook.com>
2019-06-26 21:25:59 -0500

    Created for loop to determine if guess is correct

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
+    // ASCII code for alphabet (a-z = 97-122)
+    var correctNumber = Math.floor(Math.random() * 25) + 97;
 
     $(document).on('keypress', function(event){
+        var pressedKey = event.keyCode;
+        console.log(event.key);
+        console.log(event.keyCode);
+
+        for (var i = 0; i < 9; i++){
+            if(pressedKey === correctNumber){
+                // if guess is correct
+            } else{
+                // if guess is incorrect
+            }
+        };
+
         $('#userGuesses').append(` ${event.key},`);
     })
-
-    // ASCII code for alphabet (a-z = 65-90)
-    var correctNumber = Math.floor(Math.random() * 25) + 65;
-
-    for (var i = 0; i < 9; i++){
-
-    }
 })
